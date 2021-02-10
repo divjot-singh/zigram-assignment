5116bbcfc91840ed85830bc4b6acbcd30aec2abb
divjot-bst <divjot.singh@bluestacks.com>
2021-02-10 19:51:49 +0530

    all fone

--- src/view/components/drinkCard.jsx
+++ src/view/components/drinkCard.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const DrinkCard = props => {
   let { onDrinkClick, drink = {} } = props,
-    { strDrinkThumb, strDrink, iDrink } = drink;
+    { strDrinkThumb, strDrink, idDrink } = drink;
   return (
-    <div className="drink-card" onClick={e => onDrinkClick(iDrink)}>
+    <div className="drink-card" onClick={e => onDrinkClick(idDrink)}>
       <img src={strDrinkThumb} className="drink-thumb" />
       <span className="drink-name">{strDrink}</span>
     </div>
--- src/view/index.jsx
+++ src/view/index.jsx
@@ -1,11 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment as F } from "react";
 import "./index.scss";
 import NetworkUtil from "../utils/networkutil";
 import DrinkCard from "./components/drinkCard";
 import SelectSearch, { fuzzySearch } from "react-select-search";
 import "react-select-search/style.css";
 import { useDebounce } from "use-debounce";
-import Loader from "react-loader-spinner";
+import LoadingOverlay from "react-loading-overlay";
+import { Dialog } from "@material-ui/core";
+import BounceLoader from "react-spinners/BounceLoader";
+import { FaTimes } from "react-icons/fa";
 
 const Home = props => {
   let [categories, setCategories] = useState([]),
@@ -15,7 +18,8 @@ const Home = props => {
     [searchText, setSearchText] = useState(""),
     [drinks, setDrinks] = useState([]),
     [searchString] = useDebounce(searchText, 1000),
-    [loading, setLoading] = useState(false);
+    [loading, setLoading] = useState(false),
+    [selectedDrink, setSelectedDrink] = useState({});
   useEffect(() => {
     if (!categories.length) {
       fetchCategories();
@@ -25,10 +29,11 @@ const Home = props => {
     }
   });
   const fetchCategories = async () => {
+    setLoading(true);
     let categories = await NetworkUtil.get(
       "https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list"
     );
-
+    setLoading(false);
     if (categories.drinks && categories.drinks.length) {
       setCategories(categories.drinks);
       let selectedCategory = categories.drinks[0].strCategory
@@ -52,10 +57,11 @@ const Home = props => {
     }
   };
   const fetchIngredients = async () => {
+    setLoading(true);
     let ingredients = await NetworkUtil.get(
       "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
     );
-
+    setLoading(false);
     if (ingredients.drinks) {
       setIngredients(ingredients.drinks);
     }
@@ -93,67 +99,144 @@ const Home = props => {
       setDrinks([]);
     }
   };
-  if (loading) {
-    return (
-      <div className="container loader">
-        <Loader
-          type="Puff"
-          color="#00BFFF"
-          height={100}
-          width={100}
-          timeout={3000} //3 secs
-        />
-      </div>
-    );
-  }
+
+  const onDrinkClick = async drinkId => {
+    setLoading(true);
+    let url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkId}`;
+    let details = await NetworkUtil.get(url);
+    setLoading(false);
+    if (details && details.drinks && details.drinks.length) {
+      setSelectedDrink(details.drinks[0]);
+    }
+  };
+  const getIngredients = selectedDrink => {
+    let ingredients = [];
+    for (let key in selectedDrink) {
+      if (key.indexOf("strIngredient") > -1) {
+        ingredients.push(selectedDrink[key]);
+      }
+    }
+    if (ingredients.length)
+      return (
+        <div className="ingredients">
+          <div className="label">Ingredients: </div>
+          <div className="ingredient-details">
+            {ingredients.map(ingredient => (
+              <div>{ingredient}</div>
+            ))}
+          </div>
+        </div>
+      );
+    else return "";
+  };
   return (
-    <div className="container">
-      <div className="category-selector">
-        <SelectSearch
-          options={categories.map(category => {
-            return {
-              name: category.strCategory,
-              value: category.strCategory.split(" ").join("_")
-            };
-          })}
-          search
-          filterOptions={fuzzySearch}
-          value={selectedCategory}
-          onChange={onCategoryChange}
-          name="categories"
-          placeholder="Choose a category"
-        />
-        <SelectSearch
-          options={ingredients.map(ingredient => {
-            return {
-              name: ingredient.strIngredient1,
-              value: ingredient.strIngredient1.split(" ").join("_")
-            };
-          })}
-          search
-          filterOptions={fuzzySearch}
-          value={selectedIngredient}
-          onChange={onIngredientChange}
-          name="ingredients"
-          placeholder="Choose an ingredient"
-        />
-        <input
-          className="plain-input select-search__input"
-          value={searchText}
-          onChange={onSearchTextChange}
-          placeholder="Search via name"
-        ></input>
+    <LoadingOverlay
+      className="loading-overlay"
+      active={loading}
+      spinner={<BounceLoader className="loader" />}
+    >
+      <Dialog
+        maxHeight={"80vh"}
+        maxWidth={"300px"}
+        open={Object.keys(selectedDrink).length}
+        className="drink-details-dialog"
+        // onClose={() => {
+        //   setSelectedDrink({});
+        // }}
+        aria-labelledby="max-width-dialog-title"
+      >
+        <div
+          className="cancel-icon"
+          onClick={() => {
+            setSelectedDrink({});
+          }}
+        >
+          <FaTimes />
+        </div>
+        <div className="drink-details">
+          <div className="header">
+            <img src={selectedDrink.strDrinkThumb} className="drink-thumb" />
+          </div>
+          <div className="drink-name">{selectedDrink.strDrink}</div>
+          <div className="content">
+            <div className="row">
+              <div className="label">Category: </div>
+              <span className="details">{selectedDrink.strCategory}</span>
+            </div>
+            <div className="row">
+              <div className="label">Glass: </div>
+              <span className="details">{selectedDrink.strGlass}</span>
+            </div>
+            <div className="row">
+              <div className="label">Type: </div>
+              <span className="details">{selectedDrink.strAlcoholic}</span>
+            </div>
+            <div className="row">
+              <div className="label">Category: </div>
+              <span className="details">{selectedDrink.strCategory}</span>
+            </div>
+          </div>
+          {getIngredients(selectedDrink)}
+          <div className="instructions">
+            <div className="label">Instructions:</div>
+            <div className="details">{selectedDrink.strInstructions}</div>
+          </div>
+        </div>
+      </Dialog>
+      <div className="container">
+        <div className="category-selector">
+          <SelectSearch
+            options={categories.map(category => {
+              return {
+                name: category.strCategory,
+                value: category.strCategory.split(" ").join("_")
+              };
+            })}
+            search
+            filterOptions={fuzzySearch}
+            value={selectedCategory}
+            onChange={onCategoryChange}
+            name="categories"
+            placeholder="Choose a category"
+          />
+          <SelectSearch
+            options={ingredients.map(ingredient => {
+              return {
+                name: ingredient.strIngredient1,
+                value: ingredient.strIngredient1.split(" ").join("_")
+              };
+            })}
+            search
+            filterOptions={fuzzySearch}
+            value={selectedIngredient}
+            onChange={onIngredientChange}
+            name="ingredients"
+            placeholder="Choose an ingredient"
+          />
+          <input
+            className="plain-input select-search__input"
+            value={searchText}
+            onChange={onSearchTextChange}
+            placeholder="Search via name"
+          ></input>
+        </div>
+        <div className="drink-list">
+          {drinks.length ? (
+            drinks.map(drink => {
+              return (
+                <DrinkCard
+                  drink={drink}
+                  key={drink.iDrink}
+                  onDrinkClick={onDrinkClick}
+                />
+              );
+            })
+          ) : (
+            <div className="no-drinks">No results found</div>
+          )}
+        </div>
       </div>
-      <div className="drink-list">
-        {drinks.length ? (
-          drinks.map(drink => {
-            return <DrinkCard drink={drink} key={drink.iDrink} />;
-          })
-        ) : (
-          <div className="no-drinks">No results found</div>
-        )}
-      </div>
-    </div>
+    </LoadingOverlay>
   );
 };
 
